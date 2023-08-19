4d0ef7ab9a3b112c3da33f143cd7bfeb9dd1028e
Saradha <mailtosaradha@gmail.com>
2023-08-19 18:21:30 -0500

    Exercises for understanding comments

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,7 +1,13 @@
-   // This demo shows off comments!
+  // This demo shows off comments!
 
-   // console.log("This does not print.");
+   //console.log("This does not print.");
 
+ // this is to test a single line comment
+ /* this 
+ is 
+ to 
+ test 
+ multi line*/
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
    /* Here is how
